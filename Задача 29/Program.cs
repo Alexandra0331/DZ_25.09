93c72e49161bf6ac63144825ea2e079a2d4692ca
Alexandra <Kaverzina0331@mail.ru>
2022-09-27 22:05:26 +0300

    Задача 29

---
+++ Задача 29/Program.cs
@@ -0,0 +1,18 @@
+﻿
+// Задача 29: Напишите программу, которая задаёт
+//  массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+Console.Clear();
+//Console.Write ("Введите количество элементов массива: \t");
+//int elment = int.Parse(Console.ReadLine());
+
+int[] myArray = new int[8];
+for (int i=0; i < myArray.Length; i++)
+{
+    Console.Write ($"Введите элемент массива под индексом {i}: \t");
+    myArray[i] = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine("[" + string.Join (", ", myArray) +"]");
