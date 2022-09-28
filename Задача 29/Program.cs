fdee229b010ee472a1184cfe2857044e9c93825b
Alexandra <Kaverzina0331@mail.ru>
2022-09-28 19:34:37 +0300

    Задача 29

--- Задача 29/Program.cs
+++ Задача 29/Program.cs
@@ -5,14 +5,11 @@
 // 6, 1, 33 -> [6, 1, 33]
 
 Console.Clear();
-//Console.Write ("Введите количество элементов массива: \t");
-//int elment = int.Parse(Console.ReadLine());
-
 int[] myArray = new int[8];
 for (int i=0; i < myArray.Length; i++)
 {
     Console.Write ($"Введите элемент массива под индексом {i}: \t");
-    myArray[i] = int.Parse(Console.ReadLine());
+    myArray[i] = int.Parse(Console.ReadLine()); 
 }
 
 Console.WriteLine("[" + string.Join (", ", myArray) +"]");
